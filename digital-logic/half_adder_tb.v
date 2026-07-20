d0469f61cd75a62411fd406ddca84d2aa65f1441
Aditi Pandhare <aditipandhare6@gmail.com>
2026-07-20 23:13:36 +0530

    Add testbench for half adder module

---
+++ digital-logic/half_adder_tb.v
@@ -0,0 +1,27 @@
+module half_adder_tb;
+reg a;
+reg b;
+wire sum;
+wire carry;
+half_adder uut(
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+);
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, half_adder_tb);
+    $monitor("Time=%0t A=%b B=%b Sum=%b Carry=%b",
+             $time, a, b, sum, carry);
+    a = 0; b = 0;
+    #10;
+    a = 0; b = 1;
+    #10;
+    a = 1; b = 0;
+    #10;
+    a = 1; b = 1;
+    #10;
+    $finish;
+end
+endmodule
